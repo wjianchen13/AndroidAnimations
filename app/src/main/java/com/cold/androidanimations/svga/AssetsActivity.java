4f26d97aa2349b3a216931c67cc8dbe326958b13
282330332 <282330332@qq.com>
2023-04-19 10:48:55 +0800

    svga例子

--- app/src/main/java/com/cold/androidanimations/svga/AssetsActivity.java
+++ app/src/main/java/com/cold/androidanimations/svga/AssetsActivity.java
@@ -42,7 +42,7 @@ public class AssetsActivity extends AppCompatActivity {
      * @return
      */
     public void onAssets(View v) {
-        loadAnim(sImgvTest, "ic_poker_limit.svga");
+        loadAnim(sImgvTest, "main_send_gift.svga");
     }
 
     private void loadAnim(final SVGAImageView v, String name) {
--- app/src/main/java/com/cold/androidanimations/svga/AssetsOtherActivity.java
+++ app/src/main/java/com/cold/androidanimations/svga/AssetsOtherActivity.java
@@ -1,33 +1,53 @@
 package com.cold.androidanimations.svga;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Layout;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.StaticLayout;
+import android.text.TextPaint;
+import android.text.style.ForegroundColorSpan;
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.cold.androidanimations.R;
 import com.opensource.svgaplayer.SVGACallback;
+import com.opensource.svgaplayer.SVGADrawable;
+import com.opensource.svgaplayer.SVGADynamicEntity;
 import com.opensource.svgaplayer.SVGAImageView;
 import com.opensource.svgaplayer.SVGAParser;
 import com.opensource.svgaplayer.SVGAVideoEntity;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.net.URL;
+
 public class AssetsOtherActivity extends AppCompatActivity {
 
     private SVGAImageView svgaMatchMatch;
-    private SVGAImageView sImgvTest1;
+    private SVGAImageView svgaEntry;
+    private SVGAParser svgaParser;
+    private int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assets_other);
+
+        SVGAParser.Companion.shareParser().init(this);
+        svgaParser = SVGAParser.Companion.shareParser();
+
         svgaMatchMatch = findViewById(R.id.svga_match_match);
         svgaMatchMatch.setCallback(new SVGACallback() {
             @Override
             public void onFinished() {
-                System.out.println("==========================> onFinished: ");
+//                System.out.println("==========================> onFinished: ");
             }
 
             @Override
@@ -42,21 +62,47 @@ public class AssetsOtherActivity extends AppCompatActivity {
 
             @Override
             public void onStep(int i, double v) {
-                System.out.println("==========================> i: " + i + "  v: " + v);
+//                System.out.println("==========================> i: " + i + "  v: " + v);
             }
         });
-//        svgaMatchMatch.post(new Runnable() {
-//            @Override
-//            public void run() {
-//                svgaMatchMatch.stepToPercentage(0f, false);
-//            }
-//        });
+
+        svgaEntry = findViewById(R.id.svga_entry);
+        svgaEntry.setCallback(new SVGACallback() {
+            @Override
+            public void onFinished() {
+//                System.out.println("==========================> onFinished: ");
+            }
+
+            @Override
+            public void onPause() {
+
+            }
+
+            @Override
+            public void onRepeat() {
+
+            }
+
+            @Override
+            public void onStep(int i, double v) {
+                count ++;
+                System.out.println("==========================> i: " + i + "  v: " + v + " count: " + count);
+            }
+        });
+
+
+    }
+
+    /**
+     * 启动动画
+     * @param
+     * @return
+     */
+    public void onTest1(View v) {
         loadAnim(svgaMatchMatch, "bg_match_match.svga");
     }
 
     private void loadAnim(final SVGAImageView v, String name) {
-        SVGAParser.Companion.shareParser().init(this);
-        SVGAParser svgaParser = SVGAParser.Companion.shareParser();
 //        String name = this.randomSample();
         //asset jojo_audio.svga  cannot callback
         Log.d("SVGA", "## name " + name);
@@ -77,15 +123,6 @@ public class AssetsOtherActivity extends AppCompatActivity {
         }, null);
     }
 
-    /**
-     *
-     * @param
-     * @return
-     */
-    public void onTest1(View v) {
-        svgaMatchMatch.startAnimation();
-//        svgaMatchMatch.res
-    }
 
     /**
      *
@@ -93,7 +130,7 @@ public class AssetsOtherActivity extends AppCompatActivity {
      * @return
      */
     public void onTest2(View v) {
-
+        svgaEntry.startAnimation();
     }
 
     /**
@@ -102,7 +139,7 @@ public class AssetsOtherActivity extends AppCompatActivity {
      * @return
      */
     public void onTest3(View v) {
-
+        svgaEntry.stopAnimation();
     }
 
     /**
@@ -111,7 +148,80 @@ public class AssetsOtherActivity extends AppCompatActivity {
      * @return
      */
     public void onTest4(View v) {
+        try {
+            String path = getFilesDir().getAbsolutePath();
+            String name = path + File.separator + "vip_entry_1.svga";
+            SVGAParser.Companion.shareParser().init(this);
+            SVGAParser svgaParser = SVGAParser.Companion.shareParser();
 
+            File animFile = new File(name);
+            FileInputStream fileInputStream = new FileInputStream(animFile);
+            if (svgaParser != null) {
+                svgaParser.decodeFromInputStream(fileInputStream, name, new SVGAParser.ParseCompletion() {
+                    @Override
+                    public void onComplete(SVGAVideoEntity svgaVideoEntity) {
+                        SVGADynamicEntity svgaDynamicEntity = new SVGADynamicEntity();
+                        try {
+                            svgaEntry.setVideoItem(svgaVideoEntity);
+                            svgaEntry.stepToFrame(0, true);
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+
+                        }
+                    }
+
+                    @Override
+                    public void onError() {
+
+                    }
+                }, true, null, name);
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void test() {
+        try {
+            SVGAParser.Companion.shareParser().init(this);
+            SVGAParser parser = SVGAParser.Companion.shareParser();
+            parser.parse(new URL("https://github.com/yyued/SVGA-Samples/blob/master/kingset.svga?raw=true"),
+                    new SVGAParser.ParseCompletion() {
+                        @Override
+                        public void onComplete(SVGAVideoEntity videoItem) {
+                            SVGADynamicEntity dynamicEntity = new SVGADynamicEntity();
+                            SpannableStringBuilder spannableStringBuilder = new SpannableStringBuilder("Pony sends many flowers.");
+                            spannableStringBuilder.setSpan(new ForegroundColorSpan(Color.YELLOW), 0, 4, Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+                            TextPaint textPaint = new TextPaint();
+                            textPaint.setColor(Color.WHITE);
+                            textPaint.setTextSize(28);
+                            dynamicEntity.setDynamicText(new StaticLayout(
+                                    spannableStringBuilder,
+                                    0,
+                                    spannableStringBuilder.length(),
+                                    textPaint,
+                                    0,
+                                    Layout.Alignment.ALIGN_CENTER,
+                                    1.0f,
+                                    0.0f,
+                                    false
+                            ), "banner");
+
+                            SVGADrawable drawable = new SVGADrawable(videoItem, dynamicEntity);
+                            svgaMatchMatch.setImageDrawable(drawable);
+                            svgaMatchMatch.startAnimation();
+                        }
+                        @Override
+                        public void onError() {
+
+                        }
+                    }
+            );
+        } catch (Exception e) {
+            System.out.print(true);
+        }
     }
     
 }
